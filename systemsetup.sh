0d8b8d61e498330a66322e70dd3cc44ed0c97d97
Ignacio Catalina <icatalina@gmail.com>
2015-11-17 00:41:14 +0000

    Few amends

--- systemsetup.sh
+++ systemsetup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 brewApps=(
-  binutils
-  bash
-  caskroom/cask/brew-cask
   coreutils
+  bash
+  cmake
+  caskroom/cask/brew-cask
   diffutils
   ed --default-names
   file-formula
@@ -30,9 +30,11 @@ brewApps=(
   watch
   wdiff --with-gettext
   wget
+  tmux
   the_silver_searcher
   zsh
-  macvim --with-lua --with-custom-icons --with-luajit --with-override-system-vim --HEAD
+  vim --override-system-vi --HEAD
+  reattach-to-user-namespace
 )
 
 casksApps=(
@@ -42,6 +44,7 @@ casksApps=(
   caskroom/versions/firefoxdeveloperedition
   caskroom/versions/google-chrome-canary
   firefox
+  font-hack
   google-chrome
   imageoptim
   iterm2
@@ -89,6 +92,11 @@ else
   echo "Brew already Installed..."
 fi
 
+brew tap homebrew/versions
+brew install caskroom/cask/brew-cask
+brew tap caskroom/fonts
+brew tap homebrew/dupes
+
 # Update homebrew recipes
 echo "Updating Formulas..."
 brew update
@@ -99,13 +107,45 @@ brew tap homebrew/dupes
 echo "Installing Stuff..."
 
 echo "Installing brew packages..."
-brew install ${brewApps[@]}
+# brew install ${brewApps[@]}
+brew install coreutils
+brew install bash
+brew install caskroom/cask/brew-cask
+brew install diffutils
+brew install ed --default-names
+brew install file-formula
+brew install findutils --with-default-names
+brew install gawk
+brew install git
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install less
+brew install python
+brew install python3
+brew install ngrok
+brew install node
+brew install rsync
+brew install ruby
+brew install screen
+brew install unzip
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+brew install tmux
+brew install the_silver_searcher
+brew install zsh
+brew install vim --override-system-vi --HEAD
 
 echo "Installing Casks..."
-brew cask install ${casksApps[@]}
+# brew cask install ${casksApps[@]}
 
 echo "Installing QuickLookPlugins..."
-brew cask install ${quickLookPlugins[@]}
+#brew cask install ${quickLookPlugins[@]}
 
 echo "Cleaning brew..."
 brew cleanup
