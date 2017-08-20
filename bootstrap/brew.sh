091df0e8c5f452961eaded69918bed07b73fc5e3
Ignacio Catalina <icatalina@gmail.com>
2017-08-21 08:56:14 +1000

    Add brew.sh to install dependencies

---
+++ bootstrap/brew.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env sh
+
+brew info bash                           # Bourne-Again SHell, a UNIX command interpreter
+brew info coreutils                      # GNU File, Shell, and Text utilities
+brew info diffutils                      # File comparison utilities
+brew info editorconfig                   # Maintain consistent coding style between multiple editors
+brew info file-formula                   # Utility to determine file types
+brew info findutils                      # Collection of GNU find, xargs, and locate
+brew info fswatch                        # Monitor a directory for changes and run a shell command
+brew info fzf                            # Command-line fuzzy finder written in Go
+brew info gawk                           # GNU awk utility
+brew info git                            # Distributed revision control system
+brew info gnu-indent                     # C code prettifier
+brew info gnu-sed                        # GNU implementation of the famous stream editor
+brew info gnu-tar                        # GNU version of the tar archiving utility
+brew info gnu-typist                     # GNU typing tutor
+brew info gnu-which                      # GNU implementation of which utility
+brew info gnutls                         # GNU Transport Layer Security (TLS) Library
+brew info gource                         # Version Control Visualization Tool
+brew info grep                           # GNU grep, egrep and fgrep
+brew info gzip                           # Popular GNU data compression program
+brew info highlight                      # Convert source code to formatted text with syntax highlighting
+brew info htop                           # Improved top (interactive process viewer)
+brew info hub                            # Add GitHub support to git on the command-line
+brew info jq                             # Lightweight and flexible command-line JSON processor
+brew info less                           # Pager program similar to more
+brew info ncdu                           # NCurses Disk Usage
+brew info rename                         # Perl-powered file rename script with many helpful built-ins
+brew info burntsushi/ripgrep/ripgrep-bin # Search tool like grep and The Silver Searcher.
+brew info rsync                          # Utility that provides fast incremental file transfer
+brew info ruby                           # Powerful, clean, object-oriented scripting language
+brew info screen                         # Terminal multiplexer with VT100/ANSI terminal emulation
+brew info tig                            # Text interface for Git repositories
+brew info trash                          # CLI tool that moves files or folder to the trash
+brew info tree                           # Display directories as trees (with optional color/HTML output)
+brew info unzip                          # Extraction utility for .zip compressed archives
+brew info vim                            # Vi 'workalike' with many additional features
+brew info watch                          # Executes a program periodically, showing output fullscreen
+brew info watchman                       # Watch files and take action when they change
+brew info z                              # Tracks most-used directories to make cd smarter
+brew info zsh                            # UNIX shell (command interpreter)
+
+# Browsers
+brew cask install firefox
+brew cask install firefoxdeveloperedition
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install opera
+
+# Editors
+brew cask install atom
+brew cask install visual-studio-code
+
+# DEV
+brew cask install iterm2
+brew cask install virtualbox
+
+# Social
+brew cask install nylas-mail
+brew cask install slack
+
+# Tools
+brew cask install filezilla
+brew cask install fork
+brew cask install imageoptim
+brew cask install keka
+brew cask install lastpass
+brew cask install licecap
+
+# Miscellaneous
+brew cask install colorpicker-skalacolor
+brew cask install gpgtools
+brew cask install hammerspoon
+brew cask install logitech-options
+brew cask install quicklook-csv
+brew cask install quicklook-json
+
+# Multimedia
+brew cask install vlc
+brew cask install vox
