29a472931d23a6a4f681e55672e5cba6e96c2928
Ignacio Catalina <icatalina@gmail.com>
2018-11-12 00:00:18 +1100

    hs: Refactor HJKL for arrows

--- hammerspoon/cursor_key_remap.lua
+++ hammerspoon/cursor_key_remap.lua
@@ -8,58 +8,44 @@ local function keyCode(key, modifiers)
   end
 end
 
+local function mapVimToArrowKeys(modIn, modOut)
+  modOut = modOut == nil and {} or modOut
+  hs.hotkey.bind(modIn, 'h', keyCode('left',  modOut), nil, keyCode('left',  modOut))
+  hs.hotkey.bind(modIn, 'j', keyCode('down',  modOut), nil, keyCode('down',  modOut))
+  hs.hotkey.bind(modIn, 'k', keyCode('up',    modOut), nil, keyCode('up',    modOut))
+  hs.hotkey.bind(modIn, 'l', keyCode('right', modOut), nil, keyCode('right', modOut))
+end
+
 -- Map Ctrl-[Alt]-Backspace to Delete
 hs.hotkey.bind({'ctrl'}, 'delete', keyCode('forwarddelete'), nil, keyCode('forwarddelete'))
 hs.hotkey.bind({'ctrl', 'alt'}, 'delete', keyCode('forwarddelete', {'alt'}), nil, keyCode('forwarddelete', {'alt'}))
+hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'delete', keyCode('forwarddelete', {'alt', 'shift'}), nil, keyCode('forwarddelete', {'alt', 'shift'}))
 
 -- Map Shift-Backspace to Delete
-hs.hotkey.bind({'shift'}, 'delete', keyCode('forwarddelete'), nil, keyCode('forwarddelete'))
-hs.hotkey.bind({'shift', 'alt'}, 'delete', keyCode('forwarddelete', { 'alt' }), nil, keyCode('forwarddelete'))
+-- hs.hotkey.bind({'shift'}, 'delete', keyCode('forwarddelete'), nil, keyCode('forwarddelete'))
+-- hs.hotkey.bind({'shift', 'alt'}, 'delete', keyCode('forwarddelete', { 'alt' }), nil, keyCode('forwarddelete'))
 
 -- Map Ctrl-H/J/K/L to Arrow keys
-hs.hotkey.bind({'ctrl'}, 'h', keyCode('left'), nil, keyCode('left'))
-hs.hotkey.bind({'ctrl'}, 'j', keyCode('down'), nil, keyCode('down'))
-hs.hotkey.bind({'ctrl'}, 'k', keyCode('up'), nil, keyCode('up'))
-hs.hotkey.bind({'ctrl'}, 'l', keyCode('right'), nil, keyCode('right'))
-
+mapVimToArrowKeys({'ctrl'})
 -- Map Ctrl-Shift-H/J/K/L to Shift-Arrow keys
-hs.hotkey.bind({'ctrl', 'shift'}, 'h', keyCode('left', {'shift'}), nil, keyCode('left', {'shift'}))
-hs.hotkey.bind({'ctrl', 'shift'}, 'j', keyCode('down', {'shift'}), nil, keyCode('down', {'shift'}))
-hs.hotkey.bind({'ctrl', 'shift'}, 'k', keyCode('up', {'shift'}), nil, keyCode('up', {'shift'}))
-hs.hotkey.bind({'ctrl', 'shift'}, 'l', keyCode('right', {'shift'}), nil, keyCode('right', {'shift'}))
-
+mapVimToArrowKeys({'ctrl', 'shift'}, {'shift'})
 -- Map Ctrl-Cmd-H/J/K/L to Cmd-Arrow keys
-hs.hotkey.bind({'ctrl', 'cmd'}, 'h', keyCode('left', {'cmd'}), nil, keyCode('left', {'cmd'}))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'j', keyCode('down', {'cmd'}), nil, keyCode('down', {'cmd'}))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'k', keyCode('up', {'cmd'}), nil, keyCode('up', {'cmd'}))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'l', keyCode('right', {'cmd'}), nil, keyCode('right', {'cmd'}))
-
+mapVimToArrowKeys({'ctrl', 'cmd'}, {'cmd'})
 -- Map Ctrl-Alt-H/J/K/L to Alt-Arrow keys
-hs.hotkey.bind({'ctrl', 'alt'}, 'h', keyCode('left', {'alt'}), nil, keyCode('left', {'alt'}))
-hs.hotkey.bind({'ctrl', 'alt'}, 'j', keyCode('down', {'alt'}), nil, keyCode('down', {'alt'}))
-hs.hotkey.bind({'ctrl', 'alt'}, 'k', keyCode('up', {'alt'}), nil, keyCode('up', {'alt'}))
-hs.hotkey.bind({'ctrl', 'alt'}, 'l', keyCode('right', {'alt'}), nil, keyCode('right', {'alt'}))
-
+mapVimToArrowKeys({'ctrl', 'alt'}, {'alt'})
 -- Map Ctrl-Shift-Cmd-H/J/K/L to Shift-Cmd-Arrow keys
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'h', keyCode('left', {'shift', 'cmd'}), nil, keyCode('left', {'shift', 'cmd'}))
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'j', keyCode('down', {'shift', 'cmd'}), nil, keyCode('down', {'shift', 'cmd'}))
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'k', keyCode('up', {'shift', 'cmd'}), nil, keyCode('up', {'shift', 'cmd'}))
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'l', keyCode('right', {'shift', 'cmd'}), nil, keyCode('right', {'shift', 'cmd'}))
-
+mapVimToArrowKeys({'ctrl', 'shift', 'cmd'})
 -- Map Ctrl-Shift-Alt-H/J/K/L to Shift-Alt-Arrow keys
-hs.hotkey.bind({'ctrl', 'shift', 'alt'}, 'h', keyCode('left', {'shift', 'alt'}), nil, keyCode('left', {'shift', 'alt'}))
-hs.hotkey.bind({'ctrl', 'shift', 'alt'}, 'j', keyCode('down', {'shift', 'alt'}), nil, keyCode('down', {'shift', 'alt'}))
-hs.hotkey.bind({'ctrl', 'shift', 'alt'}, 'k', keyCode('up', {'shift', 'alt'}), nil, keyCode('up', {'shift', 'alt'}))
-hs.hotkey.bind({'ctrl', 'shift', 'alt'}, 'l', keyCode('right', {'shift', 'alt'}), nil, keyCode('right', {'shift', 'alt'}))
-
+mapVimToArrowKeys({'ctrl', 'shift', 'alt'}, {'shift', 'alt'})
 -- Map Ctrl-Cmd-Alt-H/J/K/L to Ctrl-Shift-Cmd-Arrow keys
-hs.hotkey.bind({'ctrl', 'cmd', 'alt'}, 'h', keyCode('left', {'cmd', 'ctrl', 'alt'}), nil, keyCode('left', {'cmd', 'ctrl', 'alt'}))
-hs.hotkey.bind({'ctrl', 'cmd', 'alt'}, 'j', keyCode('down', {'cmd', 'ctrl', 'alt'}), nil, keyCode('down', {'cmd', 'ctrl', 'alt'}))
-hs.hotkey.bind({'ctrl', 'cmd', 'alt'}, 'k', keyCode('up', {'cmd', 'ctrl', 'alt'}), nil, keyCode('up', {'cmd', 'ctrl', 'alt'}))
-hs.hotkey.bind({'ctrl', 'cmd', 'alt'}, 'l', keyCode('right', {'cmd', 'ctrl', 'alt'}), nil, keyCode('right', {'cmd', 'ctrl', 'alt'}))
+mapVimToArrowKeys({'ctrl', 'cmd', 'alt'}, {'cmd', 'ctrl', 'alt'})
+mapVimToArrowKeys({'ctrl', 'cmd', 'alt', 'shift'}, {'cmd', 'ctrl', 'alt', 'shift'})
+
+
 
 -- KBParadise V60 Keyboard - Allow ~ with <shift-esc>
 hs.hotkey.bind({'shift'}, 'escape', keyCode('`', {'shift'}), nil, keyCode('`', {'shift'}))
+hs.hotkey.bind({'cmd'},   'escape', keyCode('`', {'cmd'}), nil, keyCode('`', {'cmd'}))
 
 hs.hotkey.bind({'ctrl'}, '\'', keyCode('`'), nil, keyCode('`'))
 hs.hotkey.bind({'ctrl', 'alt'}, '\'', keyCode('`', { 'alt' }), nil, keyCode('`', { 'alt' }))
